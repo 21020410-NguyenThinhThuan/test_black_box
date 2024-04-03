2a71271e4d51b6e476cec75465ec700e648013f8
21020410-NguyenThinhThuan <21020410@vnu.edu.vn>
2024-04-03 19:39:07 +0700

    commit

---
+++ bill.py
@@ -0,0 +1,19 @@
+def tinh_gia_tien(can_nang):
+    if can_nang > 30 and can_nang <= 40:
+        gia_tien = 20000
+    elif can_nang > 40 and can_nang <= 50:
+        gia_tien = 25000
+    elif can_nang > 50 and can_nang <= 60:
+        gia_tien = 30000
+        
+    return gia_tien
+
+# Nhập cân nặng từ người dùng
+can_nang = float(input("Nhập cân nặng (kg): "))
+
+# Tính giá tiền
+gia_tien = tinh_gia_tien(can_nang)
+
+# In ra kết quả
+print("Giá tiền: {} đồng".format(gia_tien))
+
